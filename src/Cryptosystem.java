909834a2f46cd688cf5d30dec696591b4a2046a1
Steven Chen <stevenzchen@me.com>
2015-02-11 22:40:48 -0600

    Add main class

---
+++ src/Cryptosystem.java
@@ -0,0 +1,15 @@
+
+public class Cryptosystem {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    private String encrypt(String input, String key) {
+        return "";
+    }
+    
+    private String decrypt(String ciphertext, String key) {
+        return "";
+    }
+}
